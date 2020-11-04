2a9a39613f866ed2cc4bdbe85b05a2826db8da5a
pyPlacca <dhegreatprosperity10@gmail.com>
2020-11-04 18:47:16 +0000

    simple react native view

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={require("./assets/please.jpg")} style={styles.image}/>
+      <View >
+        {
+          [
+            ['Name', 'David Placca'],
+            ['Email', 'email@email.com'],
+            ['Gender', 'Male']
+          ].map((info, i) => {
+            const [title, text] = info
+            return (
+              <View style={styles.row} key={i}>
+                <Text style={styles.title}>{title}</Text>
+                <Text style={styles.text}>{text}</Text>
+              </View>
+            )
+          })
+        }
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    paddingVertical: "20%",
   },
+
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: '5%',
+  },
+
+  row: {
+    flex: 1,
+    alignItems: 'flex-start',
+    flexDirection: 'row',
+    marginBottom: '5%',
+  },
+
+  title: {
+    marginRight: '5%',
+    width: 70,
+    borderColor: 'blue',
+    borderWidth: 1,
+  },
+
+  text: {
+    flexGrow: .6,
+    textAlign: 'left',
+    alignSelf: 'flex-start'
+  }
+
 });
